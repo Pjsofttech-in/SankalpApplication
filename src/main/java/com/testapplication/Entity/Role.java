5a810cb98a173b7ac9b9595f8eb3eb9ad40fadfc
YOGITA <pyogita568@gmail.com>
2026-07-08 12:25:44 +0530

    Initial commit

---
+++ src/main/java/com/testapplication/Entity/Role.java
@@ -0,0 +1,25 @@
+package com.testapplication.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Role
+{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "role_name", nullable = false, unique = true)
+    private String roleName;
+
+    @Column(length = 200)
+    private String description;
+}
