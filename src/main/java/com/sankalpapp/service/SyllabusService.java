8fcb7791b2000cbf5c3f033a1c8d467249f9c551
pjsofttech <pjsofttech@pjsofttech.com>
2026-08-26 18:15:19 +0530

    Syllabus file upload done

--- src/main/java/com/sankalpapp/service/SyllabusService.java
+++ src/main/java/com/sankalpapp/service/SyllabusService.java
@@ -1,18 +1,19 @@
 package com.sankalpapp.service;
 
 import com.sankalpapp.entity.Syllabus;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface SyllabusService {
 
-    Syllabus createSyllabus(Syllabus syllabus);
+    Syllabus createSyllabus(Syllabus syllabus, MultipartFile file);
 
     List<Syllabus> getAllSyllabus();
 
     Syllabus getSyllabusById(Long id);
 
-    Syllabus updateSyllabus(Long id, Syllabus syllabus);
+    Syllabus updateSyllabus(Long id, Syllabus syllabus, MultipartFile file);
 
     void deleteSyllabus(Long id);
 }
--- src/main/java/com/sankalpapp/serviceimpl/SyllabusServiceImpl.java
+++ src/main/java/com/sankalpapp/serviceimpl/SyllabusServiceImpl.java
@@ -3,22 +3,36 @@ package com.sankalpapp.serviceimpl;
 import com.sankalpapp.entity.Syllabus;
 import com.sankalpapp.repository.SyllabusRepository;
 import com.sankalpapp.service.SyllabusService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class SyllabusServiceImpl implements SyllabusService {
 
+    private static final String folder = "Syllabus";
     private final SyllabusRepository syllabusRepository;
-
-    public SyllabusServiceImpl(SyllabusRepository syllabusRepository) {
-        this.syllabusRepository = syllabusRepository;
-    }
+    private final S3Service s3service;
 
     @Override
-    public Syllabus createSyllabus(Syllabus syllabus) {
+    public Syllabus createSyllabus(Syllabus syllabus, MultipartFile file) {
+        uploadFile(file, syllabus);
         return syllabusRepository.save(syllabus);
+    }
+
+    private void uploadFile(MultipartFile pdf, Syllabus syllabus) {
+        if (pdf != null) {
+            try {
+                String fileURL = s3service.uploadFile(pdf, folder);
+                syllabus.setLink(fileURL);
+            } catch (IOException e) {
+                throw new RuntimeException("Unable to upload File");
+            }
+        }
     }
 
     @Override
@@ -35,7 +49,7 @@ public class SyllabusServiceImpl implements SyllabusService {
     }
 
     @Override
-    public Syllabus updateSyllabus(Long id, Syllabus syllabus) {
+    public Syllabus updateSyllabus(Long id, Syllabus syllabus, MultipartFile file) {
 
         Syllabus existingSyllabus = syllabusRepository.findById(id)
                 .orElseThrow(() ->
@@ -44,6 +58,7 @@ public class SyllabusServiceImpl implements SyllabusService {
 
         existingSyllabus.setTitle(syllabus.getTitle());
         existingSyllabus.setLink(syllabus.getLink());
+        uploadFile(file, syllabus);
 
         return syllabusRepository.save(existingSyllabus);
     }
@@ -55,6 +70,8 @@ public class SyllabusServiceImpl implements SyllabusService {
                 .orElseThrow(() ->
                         new RuntimeException("Syllabus not found with id: " + id)
                 );
+
+        s3service.deleteFileByUrl(existingSyllabus.getLink());
 
         syllabusRepository.delete(existingSyllabus);
     }
