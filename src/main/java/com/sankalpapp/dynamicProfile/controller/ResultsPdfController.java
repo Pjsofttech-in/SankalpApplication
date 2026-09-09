ea7584cb0edc930454e1c2386ac8ff6db667f907
pjsofttech <pjsofttech@pjsofttech.com>
2026-09-09 20:21:28 +0530

    Result pdf base url changed

--- src/main/java/com/sankalpapp/dynamicProfile/controller/ResultsPdfController.java
+++ src/main/java/com/sankalpapp/dynamicProfile/controller/ResultsPdfController.java
@@ -13,7 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/results-pdfs")
+@RequestMapping("/results-pdfs")
 @RequiredArgsConstructor
 public class ResultsPdfController {
 
